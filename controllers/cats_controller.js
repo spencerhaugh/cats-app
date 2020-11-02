f6a89a82dfee12d0e8edd0f53ccc4d6beb21ed64
Spencer Haugh <spencer.haugh@gmail.com>
2020-11-02 17:22:47 -0500

    main routes moved to controller successfully omg

---
+++ controllers/cats_controller.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const Catabase = require('../models/catabase.js');
+const catabase = express.Router();
+
+
+// ROUTES
+
+//new
+catabase.get('/new', (req, res) => {
+    // res.send('This will create a new item, eventually.');
+    res.render('new.ejs', 
+    {currentUser: req.session.currentUser}
+    );
+});
+
+
+//update/edit link (to edit page)
+catabase.get('/:id/edit', (req, res) => {
+    // res.send('This is where the edit form will be');
+    Catabase.findById(req.params.id, (err, foundCat) => {
+        res.render('edit.ejs', {
+            cat: foundCat,
+            currentUser: req.session.currentUser
+        })
+    })
+});
+//update/edit form (put route)
+catabase.put('/:id', (req, res) => {
+    Catabase.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        {new: true}, 
+        (err, updatedCat) => {
+            res.redirect('/catabase')
+        })
+})
+
+//Create (new post route)
+catabase.post('/', (req, res) => {
+    console.log('clicked post')
+    Catabase.create(req.body, (err, createCat) => {
+        res.redirect('/catabase')
+        console.log('posted post')
+        console.log(req.body)
+    })
+})
+
+//delete
+catabase.delete('/:id', async (req, res) => {
+    console.log('Delete attempt')
+    await Catabase.findByIdAndRemove(req.params.id, (err, deletedCat) => {
+        console.log('Delete success')
+        res.redirect('/catabase')
+    })
+})
+
+//show
+catabase.get('/:id', async (req, res) => {
+    // res.send('This will be the show page');
+    Catabase.findById(req.params.id, (err, foundCat) => {
+        res.render('show.ejs', {
+            cat: foundCat,
+            currentUser: req.session.currentUser
+        })
+    })
+});
+
+//index
+catabase.get('/' , (req, res) => {
+    Catabase.find({}, (err, allCats) => {
+        res.render('index.ejs', { 
+            catabase: allCats,
+            currentUser: req.session.currentUser 
+        });
+    }
+)}
+);
+
+module.exports = catabase;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require ('mongoose');
 require('dotenv').config();
 const app = express ();
 const db = mongoose.connection;
-const Catabase = require('./models/catabase.js');
+// const Catabase = require('./models/catabase.js');
 //===================
 //Port
 //===================
@@ -57,7 +57,8 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 //===================
 
 //Cats
-const catsController
+const catsController = require('./controllers/cats_controller.js');
+app.use('/catabase', catsController);
 
 //Users
 const userController = require('./controllers/users_controller.js');
@@ -72,76 +73,7 @@ app.use('/sessions', sessionsController);
 //===================
 
 
-//new
-app.get('/catabase/new', (req, res) => {
-    // res.send('This will create a new item, eventually.');
-    res.render('new.ejs', 
-    {currentUser: req.session.currentUser}
-    );
-});
 
-
-//update/edit link (to edit page)
-app.get('/catabase/:id/edit', (req, res) => {
-    // res.send('This is where the edit form will be');
-    Catabase.findById(req.params.id, (err, foundCat) => {
-        res.render('edit.ejs', {
-            cat: foundCat,
-            currentUser: req.session.currentUser
-        })
-    })
-});
-//update/edit form (put route)
-app.put('/catabase/:id', (req, res) => {
-    Catabase.findByIdAndUpdate(
-        req.params.id, 
-        req.body, 
-        {new: true}, 
-        (err, updatedCat) => {
-            res.redirect('/catabase')
-        })
-})
-
-//Create (new post route)
-app.post('/catabase', (req, res) => {
-    console.log('clicked post')
-    Catabase.create(req.body, (err, createCat) => {
-        res.redirect('/catabase')
-        console.log('posted post')
-        console.log(req.body)
-    })
-})
-
-//delete
-app.delete('/catabase/:id', async (req, res) => {
-    console.log('Delete attempt')
-    await Catabase.findByIdAndRemove(req.params.id, (err, deletedCat) => {
-        console.log('Delete success')
-        res.redirect('/catabase')
-    })
-})
-
-//show
-app.get('/catabase/:id', async (req, res) => {
-    // res.send('This will be the show page');
-    Catabase.findById(req.params.id, (err, foundCat) => {
-        res.render('show.ejs', {
-            cat: foundCat,
-            currentUser: req.session.currentUser
-        })
-    })
-});
-
-//index
-app.get('/catabase' , (req, res) => {
-    Catabase.find({}, (err, allCats) => {
-        res.render('index.ejs', { 
-            catabase: allCats,
-            currentUser: req.session.currentUser 
-        });
-    }
-)}
-);
 
 //===================
 //Listener
